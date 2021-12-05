24a13b49254061014848491b4bb79354e8dab17c
Takumu YAMZATO <e215739@ie.u-ryukyu.ac.jp>
2021-12-06 00:06:30 +0900

    second commit

--- app/src/main/java/jp/ac/uryukyu/ie/e215739/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215739/Enemy.java
@@ -1,121 +1,16 @@
 package jp.ac.uryukyu.ie.e215739;
 
-/**
- * 敵クラス。
- *  String name; //敵の名前
- *  int hitPoint; //敵のHP
- *  int attack; //敵の攻撃力
- *  boolean dead; //敵の生死状態。true=死亡。
- * Created by tnal on 2016/11/13.
- */
-public class Enemy {
-    private String name;
-    private int hitPoint;
-    private int attack;
-    private boolean dead;
-
-    /**
-     * コンストラクタ。名前、最大HP、攻撃力を指定する。
-     * @param name モンスター名
-     * @param maximumHP モンスターのHP
-     * @param attack モンスターの攻撃力
-     */
+public class Enemy extends LivingThing {
     public Enemy (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+        super(name, maximumHP, attack);
     }
 
-    /**
-     * Heroへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param hero 攻撃対象
-     */
-    public void attack(Hero hero){
-        int damage = (int)(Math.random() * attack);
-        if( !dead ){
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-            hero.wounded(damage);
-        }
-    }
-
-    /**
-     * 自身へ攻撃されたときのダメージ処理をするメソッド。
-     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
-     * @param damage 受けたダメージ
-     */
+    @Override
     public void wounded(int damage){
         hitPoint -= damage;
-        if( hitPoint < 0 ) {
+        if( hitPoint <= 0 ) {
             dead = true;
             System.out.printf("モンスター%sは倒れた。\n", name);
         }
-    }
-    
-    /**
-     * メンバ変数「name」に値を設定するメソッド
-     * @param _name　設定したいname
-     */
-    public void setName(String _name){
-        this.name = _name;
-    }
-    
-    /**
-     * メンバ変数「name」を取得するメソッド
-     * @return　取得したいname
-     */
-    public String getName(){
-        return this.name;
-    }
-
-    /**
-     * メンバ変数「hitPoint」に値を設定するメソッド
-     * @param _hitPoint　設定したいhitPoint
-     */
-    public void setHitPoint(int _hitPoint){
-        this.hitPoint = _hitPoint;
-    }
-
-    /**
-     * メンバ変数「hitPoint」を取得するメソッド
-     * @return　取得したいhitPoint
-     */
-    public int getHitPoint(){
-        return this.hitPoint;
-    }
-
-    /**
-     * メンバ変数「attack」に値を設定するメソッド
-     * @param _attack　設定したいattack
-     */
-    public void setAttack(int _attack){
-        this.attack = _attack;
-    }
-
-    /**
-     * メンバ変数「attack」を取得するメソッド
-     * @return　取得したいattack
-     */
-    public int getAttack(){
-        return this.attack;
-    }
-
-    /**
-     * メンバ変数「dead」に値を設定するメソッド
-     * @param _dead　設定したいdead
-     */
-    public void setDead(boolean _dead){
-        this.dead = _dead;
-    }
-
-    /**
-     * メンバ変数「dead」を取得するメソッド
-     * @return　取得したいdead
-     */
-    public boolean getDead(){
-        return this.dead;
-    }
-
+    }  
 }
